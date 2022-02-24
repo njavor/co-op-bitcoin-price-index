7dc41495fe90b47b5712c67fe972d190e9fa5f14
njavor <nikolettjavor@gmail.com>
2022-02-24 23:45:43 +0100

    working script

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 document.addEventListener('DOMContentLoaded', main)
 
-async function main(){
-
-    let usd = document.querySelector('usd');
-    let gbp = document.querySelector('gbp');
-    let eur = document.querySelector('eur');
-
-    let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
-    let szotar = await olvaso_fetch(url);
-    usd = szotar.bpi.USD;
-    gbp = szotar.bpi.GBP;
-    eur = szotar.bpi.EUR;
-
-    
-
-}
-
 
 async function olvaso_fetch(url){
     let promise = await fetch(url);
     let promise_json = await promise.json();
-    console.log(promise_json);
     return promise_json;
+}
+
+
+async function main(){
+    let usd = document.querySelector('#usd');
+    let gbp = document.querySelector('#gbp');
+    let eur = document.querySelector('#eur');
+
+    let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    let szotar = await olvaso_fetch(url);
+    usd.innerHTML = Math.round(szotar.bpi.USD.rate_float);
+    gbp.innerHTML = Math.round(szotar.bpi.GBP.rate_float);
+    eur.innerHTML = Math.round(szotar.bpi.EUR.rate_float);
 }
