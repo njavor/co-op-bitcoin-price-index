0717b2104b80c16252ec7e52ea36c928a03d4d65
Farkas Ükös <ukosfarkas@gmail.com>
2022-02-24 14:07:06 +0100

    valamit elkezdtem

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', updateTime())
 
+function updateTime(){
+    let currentTime = new Date()
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    let t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.innerHTML = t_str;
+}
