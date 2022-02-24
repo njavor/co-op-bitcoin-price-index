4d042e7ded94ce537d563875e8ac46924d11132e
Niki Jávor <nikolettjavor@gmail.com>
2022-02-24 14:07:06 +0100

    get elements

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', main)
 
+async function main(){
+
+    let usd = document.querySelector('usd');
+    let gbp = document.querySelector('gbp');
+    let eur = document.querySelector('eur');
+
+    let url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    let szotar = await olvaso_fetch(url);
+    usd = szotar.bpi.USD;
+    gbp = szotar.bpi.GBP;
+    eur = szotar.bpi.EUR;
+
+    
+
+}
+
+
+async function olvaso_fetch(url){
+    let promise = await fetch(url);
+    let promise_json = await promise.json();
+    console.log(promise_json);
+    return promise_json;
+}
