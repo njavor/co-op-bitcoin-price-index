de5a775a33460e88ee2bf68c9e18dc75fbb2cdd3
Farkas Ükös <ukosfarkas@gmail.com>
2022-02-25 11:26:43 +0100

    Folyton frissülő idő.

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-document.addEventListener('DOMContentLoaded', updateTime())
+document.addEventListener('DOMContentLoaded', updateTime)
 
 function updateTime(){
-    let currentTime = new Date()
-    let hours = currentTime.getHours()
-    let minutes = currentTime.getMinutes()
+    let kimeno = document.querySelector("#les");
+
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
     if (minutes < 10){
-        minutes = "0" + minutes
+        minutes = "0" + minutes;
     }
     let t_str = hours + ":" + minutes + " ";
     if(hours > 11){
         t_str += "PM";
-    } else {
+    } 
+    else {
         t_str += "AM";
     }
-    document.innerHTML = t_str;
+    kimeno.innerHTML = t_str;
 }
+setInterval(updateTime, 1000);
